f52e60253014e9fa46a9c7ca1371bef4211c2c41
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-15 22:13:05 +0530

    created 4 new components

---
+++ frontend/src/app/build-pizza/build-pizza.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-build-pizza',
+  imports: [],
+  templateUrl: './build-pizza.component.html',
+  styleUrl: './build-pizza.component.css'
+})
+export class BuildPizzaComponent {
+
+}
---
+++ frontend/src/app/order-pizza/order-pizza.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-pizza',
+  imports: [],
+  templateUrl: './order-pizza.component.html',
+  styleUrl: './order-pizza.component.css'
+})
+export class OrderPizzaComponent {
+
+}
