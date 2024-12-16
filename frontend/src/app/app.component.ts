1f2539a08d8a8d068fd0444e262f910a4f63f9e9
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-16 18:41:35 +0530

    created navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'frontend';
