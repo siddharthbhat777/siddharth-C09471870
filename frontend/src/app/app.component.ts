bd3d387a8ad67b99298f3f2003b58ad22cd15b1b
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-16 19:42:40 +0530

    setup routes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [NavbarComponent]
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OrderPizzaComponent } from './order-pizza/order-pizza.component';
+import { BuildPizzaComponent } from './build-pizza/build-pizza.component';
+import { CartComponent } from './cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home'
+    },
+    {
+        path: 'order-pizza',
+        component: OrderPizzaComponent,
+        title: 'Order Pizza'
+    },
+    {
+        path: 'build-pizza',
+        component: BuildPizzaComponent,
+        title: 'Build Pizza'
+    },
+    {
+        path: 'cart',
+        component: CartComponent,
+        title: 'Cart'
+    }
+];
