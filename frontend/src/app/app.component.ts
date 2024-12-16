9cc7d5741b26f359c1f33a0dd01439bb596dee53
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-16 23:22:10 +0530

    added shared modal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/auth/auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ModalComponent } from "../shared/modal/modal.component";
+
+@Component({
+  selector: 'app-auth',
+  imports: [ModalComponent],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css'
+})
+export class AuthComponent {
+
+}
