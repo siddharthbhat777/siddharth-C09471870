e497f8eb1cd9dff907c520a0765e30f9d4415fab
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-19 20:50:29 +0530

    basic layout of cart item created

--- frontend/src/app/build-pizza/select-pizza/pizza/pizza.component.ts
+++ frontend/src/app/build-pizza/select-pizza/pizza/pizza.component.ts
@@ -16,10 +16,6 @@ export class PizzaComponent {
     return ingredients.map((ingredient) => ingredient.tname);
   }
 
-  isIngredientArray(ingredients: string[] | Ingredient[]): ingredients is Ingredient[] {
-    return ingredients.length > 0 && (ingredients[0] as Ingredient).tname !== undefined;
-  }
-
   getExtraIngredients(): string {
     const extraIngredients = this.cartItem().extraIngredients;
     if (extraIngredients.length === 0) {
--- frontend/src/app/cart/cart-item/cart-item.component.ts
+++ frontend/src/app/cart/cart-item/cart-item.component.ts
@@ -1,12 +1,26 @@
 import { Component, input } from '@angular/core';
 import { Cart } from '../cart.model';
+import { Ingredient } from '../../build-pizza/ingredient.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.css'
 })
 export class CartItemComponent {
   cartItem = input.required<Cart>();
+
+  formattedIngredient(ingredients: Ingredient[]): string[] {
+    return ingredients.map((ingredient) => ingredient.tname);
+  }
+
+  getExtraIngredients(): string {
+    const extraIngredients = this.cartItem().extraIngredients;
+    if (extraIngredients.length === 0) {
+      return 'No ingredients added';
+    }
+    return this.formattedIngredient(extraIngredients).join(', ');
+  }
 }
