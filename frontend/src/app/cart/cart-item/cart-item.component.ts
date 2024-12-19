37be3c09edd4efacd953e6fe79999aed1f7db53c
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-19 20:14:22 +0530

    passed cart data successfully

--- frontend/src/app/cart/cart-item/cart-item.component.ts
+++ frontend/src/app/cart/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Cart } from '../cart.model';
 
 @Component({
   selector: 'app-cart-item',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './cart-item.component.css'
 })
 export class CartItemComponent {
-
+  cartItem = input.required<Cart>();
 }
--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { CartItemComponent } from "./cart-item/cart-item.component";
+import { CartService } from './cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,6 +8,16 @@ import { CartItemComponent } from "./cart-item/cart-item.component";
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  private cartService = inject(CartService);
+  private destroyRef = inject(DestroyRef);
 
+  cartItems = this.cartService.sharableCartPizzas;
+
+  ngOnInit(): void {
+      const subscription = this.cartService.getCart().subscribe({
+        error: (error) => console.log(error)
+      });
+      this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
