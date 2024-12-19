b83bbeeeffcc0b12060da46c0f1e12c30c8d153d
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-20 01:22:33 +0530

    keeping stuffs updated

--- frontend/src/app/cart/cart.service.ts
+++ frontend/src/app/cart/cart.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { Cart } from './cart.model';
 import { AuthService } from '../auth/auth.service';
-import { map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ import { map, Observable, tap } from 'rxjs';
 export class CartService {
   private cartPizzas = signal<Cart[]>([]);
   sharableCartPizzas = this.cartPizzas.asReadonly();
-  
+
   private http = inject(HttpClient);
   private authService = inject(AuthService);
 
@@ -42,20 +42,45 @@ export class CartService {
   }
 
   updateCartItem(pizzaId: string, operation: 'plus' | 'minus') {
+    const previousCart = this.cartPizzas;
+    const updatedCart = this.cartPizzas().map((item) => {
+      if (item.pizzaId === pizzaId) {
+        const newQuantity = operation === 'plus' ? item.quantity + 1 : item.quantity - 1;
+        return {
+          ...item,
+          quantity: newQuantity > 0 ? newQuantity : item.quantity
+        };
+      }
+      return item;
+    });
+    this.cartPizzas.set(updatedCart);
     return this.http.put(`http://localhost:8001/cart/update-item/${this.authService.sharableData()?._id}/${pizzaId}`, {
       operation
     }, {
       headers: {
         'Authorization': `Bearer ${this.authService.token}`
       }
-    });
+    }).pipe(
+      catchError((error) => {
+        this.cartPizzas.set(previousCart());
+        return throwError(() => new Error(error));
+      })
+    );
   }
-  
+
   deleteCartItem(pizzaId: string) {
+    const previousCart = this.cartPizzas;
+    const updatedCart = this.cartPizzas().filter((item) => item.pizzaId !== pizzaId);
+    this.cartPizzas.set(updatedCart);
     return this.http.delete(`http://localhost:8001/cart/remove-item/${this.authService.sharableData()?._id}/${pizzaId}`, {
       headers: {
         'Authorization': `Bearer ${this.authService.token}`
       }
-    });
+    }).pipe(
+      catchError((error) => {
+        this.cartPizzas.set(previousCart());
+        return throwError(() => new Error(error));
+      })
+    );
   }
 }
