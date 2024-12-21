82c851fab93889b6ed6c6669a09e90a9fcde8f88
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-21 21:56:25 +0530

    add address feature added

--- backend/authentication/controllers/profile.js
+++ backend/authentication/controllers/profile.js
@@ -19,3 +19,32 @@ exports.profileEdit = async (req, res, next) => {
         next(error);
     }
 };
+
+exports.addAddress = async (req, res, next) => {
+    const userId = req.params.userId;
+    const allowedFields = ["title", "receiverName", "receiverPhone", "addressLine", "pincode", "city", "state"];
+    const newAddress = {};
+    for (const key of allowedFields) {
+        if (req.body[key] !== undefined) {
+            newAddress[key] = req.body[key];
+        }
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+                $push: { "contactDetails.addresses": newAddress }
+            },
+            { new: true, runValidators: true }
+        );
+        if (!updatedUser) {
+            const error = new Error('User not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(201).json({ message: 'Added address successfully', user: updatedUser });
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+};
--- backend/authentication/routers/profile.js
+++ backend/authentication/routers/profile.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { profileEdit } = require('../controllers/profile');
+const { profileEdit, addAddress } = require('../controllers/profile');
 
 const router = express.Router();
 
 // PUT: /profile/edit/:userId
 router.put('/edit/:userId', profileEdit);
+
+// POST: /profile/add-address/:userId
+router.put('/add-address/:userId', addAddress);
 
 module.exports = router;
