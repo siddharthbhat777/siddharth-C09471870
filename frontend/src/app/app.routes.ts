4a3f3920a37be8eec264489d6252b28225ce5178
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-23 02:06:17 +0530

    basic component setup done

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { AuthGuardService } from './auth/auth-guard.service';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { routes as buildRoutes } from './build-pizza/build.routes';
 import { ProfileComponent } from './profile/profile.component';
+import { HistoryComponent } from './history/history.component';
 
 export const routes: Routes = [
     {
@@ -39,6 +40,12 @@ export const routes: Routes = [
         component: ProfileComponent,
         canActivate: [AuthGuardService],
         title: 'Profile'
+    },
+    {
+        path: 'history',
+        component: HistoryComponent,
+        canActivate: [AuthGuardService],
+        title: 'Order history'
     },
     {
         path: '**',
