7d4bfc0655f3c363259f3110575bf4e0d08e1495
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-23 15:15:06 +0530

    added order api call

--- backend/orders/controllers/order.js
+++ backend/orders/controllers/order.js
@@ -1,3 +1,4 @@
+const Cart = require("../models/cart");
 const Order = require("../models/order");
 
 exports.getOrders = async (_, res, next) => {
@@ -36,9 +37,62 @@ exports.getOrders = async (_, res, next) => {
 };
 
 exports.addOrder = async (req, res, next) => {
+    const { userId, finalTotal, address, cartItems } = req.body;
     try {
-        const order = await Order.create(req.body);
-        res.status(200).json({ message: 'Order added successfully', order });
+        const transformedCartItems = cartItems.map(item => ({
+            quantity: item.quantity,
+            pizzaId: item.pizzaId,
+            pizzaData: {
+                name: item.name,
+                type: item.type,
+                price: item.price,
+                image: item.image,
+                description: item.description,
+                ingredients: item.ingredients,
+                extraIngredients: item.extraIngredients.map(extra => ({
+                    _id: extra._id,
+                    tname: extra.tname,
+                    price: extra.price,
+                    image: extra.image
+                })),
+                topping: item.topping
+            }
+        }));
+        const orderData = {
+            userId,
+            finalTotal,
+            address,
+            cartItems: transformedCartItems
+        };
+        const savedOrder = await Order.create(orderData);
+        await Cart.deleteOne({ userId });
+        const responseCartItems = savedOrder.cartItems.map(item => ({
+            quantity: item.quantity,
+            pizzaId: item.pizzaId.toString(),
+            name: item.pizzaData.name,
+            type: item.pizzaData.type,
+            price: item.pizzaData.price,
+            image: item.pizzaData.image,
+            description: item.pizzaData.description,
+            ingredients: item.pizzaData.ingredients,
+            extraIngredients: item.pizzaData.extraIngredients.map(extra => ({
+                _id: extra._id.toString(),
+                tname: extra.tname,
+                price: extra.price,
+                image: extra.image
+            })),
+            topping: item.pizzaData.topping
+        }));
+        const responseOrder = {
+            _id: savedOrder._id.toString(),
+            userId: savedOrder.userId.toString(),
+            finalTotal: savedOrder.finalTotal,
+            address: savedOrder.address,
+            cartItems: responseCartItems,
+            createdAt: savedOrder.createdAt,
+            updatedAt: savedOrder.updatedAt
+        };
+        res.status(200).json({ message: 'Order added successfully', order: responseOrder });
     } catch (error) {
         if (!error.statusCode) {
             error.statusCode = 500;
--- frontend/src/app/history/history.service.ts
+++ frontend/src/app/history/history.service.ts
@@ -20,4 +20,14 @@ export class HistoryService {
       map((res) => res.orders)
     );
   }
+
+  addOrder(order: History) {
+    return this.http.post<{ order: History }>('http://localhost:8001/order/add', order, {
+      headers: {
+        'Authorization': `Bearer ${this.authService.token}`
+      }
+    }).pipe(
+      map((res) => res.order)
+    );
+  }
 }
