9de778ec8143fb23ab8123faaba14e12b37f09fc
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-23 17:08:23 +0530

    added success page

--- frontend/src/app/order-successful/order-successful.component.ts
+++ frontend/src/app/order-successful/order-successful.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-order-successful',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './order-successful.component.html',
   styleUrl: './order-successful.component.css'
 })
 export class OrderSuccessfulComponent {
-
 }
