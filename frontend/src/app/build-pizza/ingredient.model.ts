65df0da43beb98e98548d5c1d356f55aee0a1a5a
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-27 19:52:19 +0530

    added auth

--- frontend/src/app/build-pizza/ingredient.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Ingredient {
-    _id: string;
-    tname: string;
-    price: number;
-    image: string;
-}
--- frontend/src/app/shared/screen-loader/screen-loader.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-screen-loader',
-  templateUrl: './screen-loader.component.html',
-  styleUrl: './screen-loader.component.css'
-})
-export class ScreenLoaderComponent { }
